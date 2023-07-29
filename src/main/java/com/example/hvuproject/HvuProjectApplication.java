54a12c08ad5e93af04634a89bf1381d78ef7c1fa
rievvy <thiagoalmeidadepaiva@gmail.com>
2023-07-29 20:29:08 -0300

    first commit

---
+++ src/main/java/com/example/hvuproject/HvuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hvuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvuProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvuProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hvuproject/HvuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hvuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvuProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
