6bcf4c09ba3ffbb77206a5effa491f4798827ed5
LuoLuann <luo.luann@gmail.com>
2023-09-28 02:22:48 -0300

    funcinou

--- hvu/src/main/java/br/ufape/lmts/hvu/model/Diretor.java
+++ hvu/src/main/java/br/ufape/lmts/hvu/model/Diretor.java
@@ -1,8 +1,12 @@
 package br.ufape.lmts.hvu.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Diretor")
 public  class Diretor extends Usuario {
 	private String matricula;
 
